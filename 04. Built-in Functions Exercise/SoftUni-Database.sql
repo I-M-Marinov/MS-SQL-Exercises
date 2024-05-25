066926c2f4a758bc29dc2b805e95600843cf6cdb
I-M-Marinov <lcfrrr@gmail.com>
2024-05-25 14:04:20 +0300

    Exercises 8, 9 and 10 solved
    Exercises 8, 9 and 10 solved

--- 04. Built-in Functions Exercise/SoftUni-Database.sql
+++ 04. Built-in Functions Exercise/SoftUni-Database.sql
@@ -4932,3 +4932,41 @@ Example
 				 Stuart				Munson
 */
 
+CREATE VIEW [V_EmployeesHiredAfter2000] AS
+SELECT FirstName, LastName FROM Employees
+WHERE (YEAR(HireDate) > 2000);
+
+SELECT * FROM V_EmployeesHiredAfter2000
+
+/*
+9.	Length of Last Name
+Create a SQL query that finds all employees, whose last name is exactly 5 characters long.
+Example
+				FirstName		LastName
+				 Kevin			 Brown
+				 Terri			 Duffy
+				  Jo			 Brown
+				 Diane			 Glimp
+*/
+
+SELECT FirstName,LastName FROM Employees
+WHERE LEN(LastName) = 5;
+
+/*
+10.	Rank Employees by Salary
+Write a query that ranks all employees using DENSE_RANK. In the DENSE_RANK function, 
+employees need to be partitioned by Salary and ordered by EmployeeID. You need to find only the employees, 
+whose Salary is between 10000 and 50000 and order them by Salary in descending order.
+Example
+			EmployeeID		FirstName		LastName		Salary		Rank
+				268			Stephen			 Jiang			48100.00	 1
+				284			Amy				 Alberts		48100.00	 2
+				288			Syed			 Abbas			48100.00	 3
+*/
+
+SELECT EmployeeID, FirstName, LastName, Salary,
+       DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS Rank
+FROM Employees
+WHERE Salary BETWEEN 10000 AND 50000
+ORDER BY Salary DESC, EmployeeID;
+
