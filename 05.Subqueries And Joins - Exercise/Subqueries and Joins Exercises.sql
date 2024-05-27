f5601cfff963c6091eb3df26b58c1e3384cbc433
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2024-05-27 18:06:26 +0300

    Exercise 3 Solved
    Exercise 3 Solved

--- 05.Subqueries And Joins - Exercise/Subqueries and Joins Exercises.sql
+++ 05.Subqueries And Joins - Exercise/Subqueries and Joins Exercises.sql
@@ -43,3 +43,23 @@ JOIN Addresses as ad ON e.AddressID = ad.AddressID
 JOIN Towns as t ON ad.TownID = t.TownID
 ORDER BY e.FirstName, e.LastName
 
+/*
+3.	Sales Employee
+Create a query that selects:
+•	EmployeeID
+•	FirstName
+•	LastName
+•	DepartmentName
+Sort them by EmployeeID in ascending order. Select only employees from the "Sales" department.
+Example
+
+		EmployeeID		FirstName		LastName		DepartmentName
+		268				Stephen			Jiang				Sales
+		273				Brian			Welcker				Sales
+
+*/
+
+SELECT e.EmployeeID,e.FirstName,e.LastName, d.Name as DepartmentName FROM Employees AS e
+JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+WHERE d.Name = 'Sales'
+ORDER BY e.EmployeeID
