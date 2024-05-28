c30a12b85cc9e1f95a82e3edfc493792bc4b5253
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2024-05-28 11:01:24 +0300

    Exercise 9 Solved
    Exercise 9 Solved

--- 05.Subqueries And Joins - Exercise/Subqueries and Joins Exercises.sql
+++ 05.Subqueries And Joins - Exercise/Subqueries and Joins Exercises.sql
@@ -181,3 +181,25 @@ SELECT
 JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
 JOIN Projects AS pr ON ep.ProjectID = pr.ProjectID
 WHERE e.EmployeeID = 24;
+
+/*
+9.	Employee Manager
+Create a query that selects:
+•	EmployeeID
+•	FirstName
+•	ManagerID
+•	ManagerName
+Filter all employees with a manager who has ID equals to 3 or 7. Return all the rows, sorted by EmployeeID in ascending order.
+Example
+
+			EmployeeID	FirstName	ManagerID	ManagerName
+				4			Rob			3		Roberto
+				9			Gail		3		Roberto
+*/
+
+SELECT e.EmployeeID, e.FirstName, e.ManagerID, m.FirstName AS ManagerName 
+FROM Employees AS e
+JOIN Employees AS m 
+ON e.ManagerID = m.EmployeeID
+WHERE m.EmployeeID IN (3,7)
+ORDER BY e.EmployeeID
