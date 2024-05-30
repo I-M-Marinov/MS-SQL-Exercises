a4e94671c2ccd7dcd6d8d1023b8c8342b843eefe
I-M-Marinov <lcfrrr@gmail.com>
2024-05-30 21:00:00 +0300

    Exercise 3 Solved
    Exercise 3 Solved

--- 06. Indices and Data Aggregation - Exercise/Indices and Data Aggregation.sql
+++ 06. Indices and Data Aggregation - Exercise/Indices and Data Aggregation.sql
@@ -27,3 +27,14 @@ Example
 
 SELECT MAX(MagicWandSize)AS [LongestMagicWand] FROM WizzardDeposits
 
+
+/*
+3. Longest Magic Wand Per Deposit Groups
+For wizards in each deposit group show the longest magic wand. Rename the new column appropriately.
+Example
+
+			DepositGroup	LongestMagicWand
+			Blue Phoenix		  31
+
+*/
+
