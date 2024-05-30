8707fdc3210170c29e709c0190136378af3111df
I-M-Marinov <lcfrrr@gmail.com>
2024-05-30 20:24:18 +0300

    Exercise 2 Solved
    Exercise 2 Solved

--- 06. Indices and Data Aggregation - Exercise/Indices and Data Aggregation.sql
+++ 06. Indices and Data Aggregation - Exercise/Indices and Data Aggregation.sql
@@ -1,5 +1,7 @@
 /*			Part I – Queries for Gringotts Database			*/
 
+SELECT * FROM WizzardDeposits
+
 /*
 1. Records' Count
 Import the database and send the total count of records from the one and only table to Mr. Bodrog. Make sure nothing gets lost.
@@ -12,4 +14,16 @@ Example
 
 SELECT COUNT(Id) AS [Count] FROM WizzardDeposits
 
+
+/*
+2. Longest Magic Wand
+Select the size of the longest magic wand. Rename the new column appropriately.
+Example
+
+		LongestMagicWand
+			  31
+*/
+
+
+SELECT MAX(MagicWandSize)AS [LongestMagicWand] FROM WizzardDeposits
 
