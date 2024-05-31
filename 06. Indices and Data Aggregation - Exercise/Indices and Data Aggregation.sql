f991ec4976feb35e5e713f42c99a116988c1e39c
I-M-Marinov <lcfrrr@gmail.com>
2024-05-31 16:54:11 +0300

    Exercise 10 Solved
    Exercise 10 Solved

--- 06. Indices and Data Aggregation - Exercise/Indices and Data Aggregation.sql
+++ 06. Indices and Data Aggregation - Exercise/Indices and Data Aggregation.sql
@@ -158,3 +158,21 @@ SELECT AgeGroup, COUNT(*) AS WizardCount
 FROM AgeGrouped
 GROUP BY AgeGroup
 ORDER BY AgeGroup;
+
+/*
+10. First Letter
+Create a query that returns all the unique wizard first letters of their first names only if they have deposit of type Troll Chest. 
+Order them alphabetically. Use GROUP BY for uniqueness.
+
+Example
+
+		FirstLetter
+			A
+			…
+*/
+
+SELECT LEFT(FirstName, 1) AS FirstLetter
+FROM WizzardDeposits
+WHERE DepositGroup = 'Troll Chest'
+GROUP BY LEFT(FirstName, 1)
+ORDER BY FirstLetter
