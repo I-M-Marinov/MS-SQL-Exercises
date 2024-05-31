dfd7383c4c6dd921eee112860ba520eccf0910e4
I-M-Marinov <lcfrrr@gmail.com>
2024-05-31 14:47:14 +0300

    Exercise 9 Solved
    Exercise 9 Solved

--- 06. Indices and Data Aggregation - Exercise/Indices and Data Aggregation.sql
+++ 06. Indices and Data Aggregation - Exercise/Indices and Data Aggregation.sql
@@ -119,3 +119,42 @@ GROUP BY w.DepositGroup, w.MagicWandCreator
 ORDER BY w.MagicWandCreator, w.DepositGroup
 
 
+/*
+9. Age Groups
+Write down a query that creates 7 different groups based on their age.
+Age groups should be as follows:
+•	[0-10]
+•	[11-20]
+•	[21-30]
+•	[31-40]
+•	[41-50]
+•	[51-60]
+•	[61+]
+
+The query should return
+•	Age groups
+•	Count of wizards in it
+Example
+
+	AgeGroup	WizardCount
+	[11-20]			21
+
+*/
+
+WITH AgeGrouped AS ( -- Common Table Expression --
+    SELECT 
+        CASE
+            WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+            WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+            WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+            WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+            WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+            WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+            WHEN Age >= 61 THEN '[61+]'
+        END AS AgeGroup
+    FROM WizzardDeposits
+)
+SELECT AgeGroup, COUNT(*) AS WizardCount
+FROM AgeGrouped
+GROUP BY AgeGroup
+ORDER BY AgeGroup;
