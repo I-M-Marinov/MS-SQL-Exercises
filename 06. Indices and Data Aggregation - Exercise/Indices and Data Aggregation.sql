59754a70f911a08f8ed7e550fc8bb663bd030174
I-M-Marinov <lcfrrr@gmail.com>
2024-06-02 00:09:38 +0300

    Exercise 14 Solved
    Exercise 14 Solved

--- 06. Indices and Data Aggregation - Exercise/Indices and Data Aggregation.sql
+++ 06. Indices and Data Aggregation - Exercise/Indices and Data Aggregation.sql
@@ -241,3 +241,21 @@ DepartmentID	TotalSalary
 SELECT e.DepartmentID, SUM(e.Salary) AS TotalSalary FROM Employees AS e
 GROUP BY e.DepartmentID
 ORDER BY  e.DepartmentID;
+
+
+/*
+14. Employees Minimum Salaries
+Select the minimum salary from the employees for departments with ID (2, 5, 7) but only for those, hired after 01/01/2000.
+Your query should return:	
+•	DepartmentID
+Example
+
+DepartmentID	MinimumSalary
+	2			25000.00
+	5			12800.00
+*/
+
+SELECT DepartmentID, MIN(Salary) AS MinimumSalary
+FROM Employees
+WHERE DepartmentID IN (2, 5, 7) AND HireDate > '2000-01-01'
+GROUP BY DepartmentID;
