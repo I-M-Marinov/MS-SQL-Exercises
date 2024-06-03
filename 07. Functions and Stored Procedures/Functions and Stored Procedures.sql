b936ee3aca5c115be5522bc867116f6f0253a928
I-M-Marinov <lcfrrr@gmail.com>
2024-06-03 18:11:36 +0300

    Exercise 2 Solved
    Exercise 2 Solved

--- 07. Functions and Stored Procedures/Functions and Stored Procedures.sql
+++ 07. Functions and Stored Procedures/Functions and Stored Procedures.sql
@@ -21,3 +21,29 @@ BEGIN
 END;
 
 EXEC usp_GetEmployeesSalaryAbove35000
+
+/*
+2.	Employees with Salary Above Number
+Create a stored procedure usp_GetEmployeesSalaryAboveNumber that accepts a number (of type DECIMAL(18,4)) 
+as parameter and returns all employees' first and last names, whose salary is above or equal to the given number. 
+
+Example
+
+Supplied number for that example is 48100.
+
+		First Name	Last Name
+		  Terri		Duffy
+		  Jean		Trenary
+		  Ken		Sanchez
+*/
+
+CREATE PROCEDURE usp_GetEmployeesSalaryAboveNumber
+@SalaryInput DECIMAL(18,4)
+AS
+BEGIN
+    SELECT FirstName, LastName
+    FROM Employees
+    WHERE Salary >= @SalaryInput;
+END;
+
+EXEC usp_GetEmployeesSalaryAboveNumber @SalaryInput = 123.4567;
