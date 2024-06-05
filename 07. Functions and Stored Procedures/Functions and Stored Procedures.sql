8fbc4c34dd40794bac157f28fdbd0826a042a498
I-M-Marinov <lcfrrr@gmail.com>
2024-06-05 10:18:16 +0300

    Exercise 9 Solved
    Exercise 9 Solved

--- 07. Functions and Stored Procedures/Functions and Stored Procedures.sql
+++ 07. Functions and Stored Procedures/Functions and Stored Procedures.sql
@@ -254,3 +254,22 @@ BEGIN
 	WHERE DepartmentID = @departmentId
 
 END;
+
+				/*                   Part II – Queries for Bank Database                     */
+
+
+/*
+9.	Find Full Name
+
+You are given a database schema with tables:
+AccountHolders(Id (PK), FirstName, LastName, SSN) and Accounts(Id (PK), AccountHolderId (FK), Balance).  
+Write a stored procedure usp_GetHoldersFullName that selects the full name of all people. 
+
+Example
+			Full Name
+			Susan Cane
+			Kim Novac
+			Jimmy Henderson
+*/
+
+
