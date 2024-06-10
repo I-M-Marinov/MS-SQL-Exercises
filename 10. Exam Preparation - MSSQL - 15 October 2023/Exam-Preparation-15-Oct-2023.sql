4d0a76139592f5914a898822fd5681d4d5bf9441
I-M-Marinov <lcfrrr@gmail.com>
2024-06-10 14:08:10 +0300

    Exercise 1 Solved
    Exercise 1 Solved

---
+++ 10. Exam Preparation - MSSQL - 15 October 2023/Exam-Preparation-15-Oct-2023.sql
@@ -0,0 +1,65 @@
+CREATE DATABASE TouristAgency 
+GO
+USE TouristAgency
+
+CREATE TABLE Countries (
+Id INT PRIMARY KEY IDENTITY,
+[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Destinations (
+Id INT PRIMARY KEY IDENTITY,
+[Name] VARCHAR(50) NOT NULL,
+CountryId INT NOT NULL,
+CONSTRAINT FK_Destinations_Countries FOREIGN KEY (CountryId) REFERENCES Countries(Id)
+)
+
+CREATE TABLE Rooms (
+Id INT PRIMARY KEY IDENTITY,
+[Type] VARCHAR(40) NOT NULL,
+Price DECIMAL(18,2) NOT NULL,
+BedCount INT CHECK (BedCount > 0 AND BedCount <= 10) NOT NULL
+)
+
+CREATE TABLE Hotels (
+Id INT PRIMARY KEY IDENTITY,
+[Name] VARCHAR(50) NOT NULL,
+DestinationId INT NOT NULL,
+CONSTRAINT FK_Hotels_Destinations FOREIGN KEY (DestinationId) REFERENCES Destinations(Id)
+)
+
+CREATE TABLE Tourists (
+Id INT PRIMARY KEY IDENTITY,
+[Name] NVARCHAR(80) NOT NULL,
+PhoneNumber VARCHAR(20) NOT NULL,
+Email VARCHAR(80) NOT NULL,
+CountryId INT NOT NULL,
+CONSTRAINT FK_Tourists_Countries FOREIGN KEY (CountryId) REFERENCES Countries(Id)
+)
+
+CREATE TABLE Bookings (
+Id INT PRIMARY KEY IDENTITY,
+ArrivalDate DATETIME2 NOT NULL,
+DepartureDate DATETIME2 NOT NULL,
+AdultsCount INT CHECK (AdultsCount >= 1 AND AdultsCount <= 10) NOT NULL,
+ChildrenCount INT CHECK (ChildrenCount >= 0 AND ChildrenCount <= 9) NOT NULL,
+TouristId INT NOT NULL, 
+HotelId INT NOT NULL, 
+RoomId INT NOT NULL,
+CONSTRAINT FK_Bookings_Tourists FOREIGN KEY (TouristId) REFERENCES Tourists(Id),
+CONSTRAINT FK_Bookings_Hotels FOREIGN KEY (HotelId) REFERENCES Hotels(Id),
+CONSTRAINT FK_Bookings_Rooms FOREIGN KEY (RoomId) REFERENCES Rooms(Id)
+)
+
+CREATE TABLE HotelsRooms (
+HotelId INT NOT NULL,
+RoomId INT NOT NULL,
+CONSTRAINT PK_HotelsRooms PRIMARY KEY (HotelId, RoomId),
+CONSTRAINT FK_HotelsRoomss_Hotels FOREIGN KEY (HotelId) REFERENCES Hotels(Id),
+CONSTRAINT FK_HotelsRooms_Rooms FOREIGN KEY (RoomId) REFERENCES Rooms(Id)
+)
+
+
+
+
+
