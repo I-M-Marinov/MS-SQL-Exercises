f790215c7c3314f9df07ebe8c630dcf7ec598126
I-M-Marinov <lcfrrr@gmail.com>
2024-06-10 10:30:26 +0300

    Exercise 2 Solved
    Exercise 2 Solved

--- 09. Exam Preparation - MSSQL - 05 December 2023/Exam-Preparation-05-Dec-2023.sql
+++ 09. Exam Preparation - MSSQL - 05 December 2023/Exam-Preparation-05-Dec-2023.sql
@@ -76,5 +76,77 @@ REFERENCES Passengers(Id)
 )
 
 
+/*
 
+2.	Insert
+Let's insert some sample data into the database. Write a query to add the following records into the corresponding tables. 
+All IDs (Primary Keys) should be auto-generated.
+					Trains
+
+HourOfDeparture	HourOfArrival	DepartureTownId	ArrivalTownId
+'07:00'			'19:00'					1				3
+'08:30'			'20:30'					5				6
+'09:00'			'21:00'					4				8
+'06:45'			'03:55'					27				7
+'10:15'			'12:15'					15				5
+
+
+TrainsRailwayStations
+TrainId	RailwayStationId	TrainId	RailwayStationId	TrainId	RailwayStationId
+36				1			37			60			       39			3
+36				4			37			16				   39			31
+36				31			38			10				   39			19
+36				57			38			50				   40			41
+36				7			38			52				   40			7
+37				13			38			22				   40			52
+37				54			39			68				   40			13
+
+
+Tickets
+Price	DateOfDeparture	DateOfArrival	TrainId	PassengerId
+90.00	'2023-12-01'	'2023-12-01'	  36			1
+115.00	'2023-08-02'	'2023-08-02'	  37			2
+160.00	'2023-08-03'	'2023-08-03'	  38			3
+255.00	'2023-09-01'	'2023-09-02'	  39			21
+95.00	'2023-09-02'	'2023-09-03'	  40			22
+
+
+*/
+
+INSERT INTO Trains (HourOfDeparture,HourOfArrival,DepartureTownId,ArrivalTownId)
+VALUES ('07:00', '19:00', 1, 3),
+	   ('08:30', '20:30', 5, 6),
+	   ('09:00', '21:00', 4, 8),
+	   ('06:45', '03:55', 27, 7),
+	   ('10:15', '12:15', 15, 5)
+
+INSERT INTO TrainsRailwayStations (TrainId,RailwayStationId)
+VALUES (36, 1),
+	   (36, 4),
+	   (36, 31),
+	   (36, 57),
+	   (36, 7),
+	   (37, 13),
+	   (37, 54),
+	   (37, 60),
+	   (37, 16),
+	   (38, 10),
+	   (38, 50),
+	   (38, 52),
+	   (38, 22),
+	   (39, 68),
+	   (39, 3),
+	   (39, 31),
+	   (39, 19),
+	   (40, 41),
+	   (40, 7),
+	   (40, 52),
+	   (40, 13)
+
+INSERT INTO Tickets(Price,DateOfDeparture,DateOfArrival,TrainId,PassengerId)
+VALUES (90.00, '2023-12-01', '2023-12-01', 36, 1),
+	   (115.00, '2023-08-02', '2023-08-02', 37, 2),
+	   (160.00, '2023-08-03', '2023-08-03', 38, 3),
+	   (255.00, '2023-09-01', '2023-09-02', 39, 21),
+	   (95.00, '2023-09-02', '2023-09-03', 40, 22)
 
