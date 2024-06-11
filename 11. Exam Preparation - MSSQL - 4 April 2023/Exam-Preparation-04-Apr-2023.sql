4798a9064716610a020445f28656e3a69b69e659
I-M-Marinov <lcfrrr@gmail.com>
2024-06-11 13:21:59 +0300

    Exercise 2 Solved
    Exercise 2 Solved

--- 11. Exam Preparation - MSSQL - 4 April 2023/Exam-Preparation-04-Apr-2023.sql
+++ 11. Exam Preparation - MSSQL - 4 April 2023/Exam-Preparation-04-Apr-2023.sql
@@ -87,8 +87,45 @@ CONSTRAINT FK_ProductsClients_Clients FOREIGN KEY (ClientId) REFERENCES Clients(
 )
 
 
+/*
+Section 2. DML (10 pts)
+Before you start, you have to import "Dataset.sql ". 
+If you have created the structure correctly, the data should be successfully inserted.
+In this section, you have to do some data manipulations:
+
+2.	Insert
+Let's insert some sample data into the database. Write a query to add the following records into the corresponding tables. All IDs should be auto-generated.
+													
+													Products
+
+				Name							Price			CategoryId		VendorId
+				SCANIA Oil Filter XD01			78.69				1				1
+				MAN Air Filter XD01				97.38				1				5
+				DAF Light Bulb 05FG87			55.00				2				13
+				ADR Shoes 47-47.5				49.85				3				5
+				Anti-slip pads S				5.87				5				7
 
 
+													Invoices
 
+				Number			IssueDate			DueDate				Amount		Currency	ClientId
+				1219992181		2023-03-01			2023-04-30			180.96			BGN			3
+				1729252340		2022-11-06			2023-01-04			158.18			EUR			13
+				1950101013		2023-02-17			2023-04-18			615.15			USD			19
+
+*/
+
+
+INSERT INTO Products ([Name],Price,CategoryId,VendorId)
+VALUES ('SCANIA Oil Filter XD01', 78.69, 1, 1),
+	   ('MAN Air Filter XD01', 97.38, 1, 5),
+	   ('DAF Light Bulb 05FG87', 55.00, 2, 13),
+	   ('ADR Shoes 47-47.5', 49.85, 3, 5),
+	   ('Anti-slip pads S', 5.87, 5, 7)
+
+INSERT INTO Invoices (Number,IssueDate,DueDate,Amount,Currency,ClientId)
+VALUES (1219992181, '2023-03-01', '2023-04-30', 180.96, 'BGN', 3),
+	   (1729252340, '2022-11-06', '2023-01-04', 158.18, 'EUR', 13),
+	   (1950101013, '2023-02-17', '2023-04-18', 615.15, 'USD', 19)
 
 
