b002bd3d3e55879fe275af5ee8b75e715b081e72
I-M-Marinov <lcfrrr@gmail.com>
2024-06-12 10:46:47 +0300

    Exercise 2 Solved
    Exercise 2 Solved

--- 12. Exam Preparation - MSSQL - 19 February 2023/Exam-Preparation-19-Feb-2023.sql
+++ 12. Exam Preparation - MSSQL - 19 February 2023/Exam-Preparation-19-Feb-2023.sql
@@ -73,3 +73,33 @@ CONSTRAINT PK_CreatorsBoardgames PRIMARY KEY (CreatorId, BoardgameId),
 CONSTRAINT FK_CreatorsBoardgames_Creators FOREIGN KEY (CreatorId) REFERENCES Creators(Id),
 CONSTRAINT FK_CreatorsBoardgames_Boardgames FOREIGN KEY (BoardgameId) REFERENCES Boardgames(Id)
 )
+
+/*
+Section 2. DML (10 pts)
+Before you start, you have to import "Dataset.sql ". If you have created the structure correctly, the data should be successfully inserted.
+In this section, you have to do some data manipulations:
+2.	Insert
+Let's insert some sample data into the database. Write a query to add the following records into the corresponding tables. All IDs should be auto-generated.
+Boardgames
+
+Name			YearPublished			Rating			CategoryId			PublisherId			PlayersRangeId
+Deep Blue			2019					5.67			1					15					7
+Paris				2016					9.78			7					1					5
+Catan: Starfarers	2021					9.87			7					13					6
+Bleeding Kansas		2020					3.25			3					7					4
+One Small Step		2019					5.75			5					9					2
+
+
+*/
+
+INSERT INTO Boardgames ([Name],YearPublished,Rating,CategoryId,PublisherId, PlayersRangeId)
+VALUES ('Deep Blue', 2019, 5.67, 1, 15, 7),
+	   ('Paris', 2016, 9.78, 7, 1, 5),
+	   ('Catan: Starfarers', 2021, 9.87, 7, 13, 6),
+	   ('Bleeding Kansas', 2020, 3.25, 3, 7, 4),
+	   ('One Small Step', 2019, 5.75, 5, 9, 2)
+
+INSERT INTO Publishers([Name],AddressId,Website,Phone)
+VALUES ('Agman Games', 5, 'www.agmangames.com', '+16546135542'),
+       ('Amethyst Games', 7, 'www.amethystgames.com', '+15558889992'),
+       ('BattleBooks', 13, 'www.battlebooks.com', '+12345678907')
