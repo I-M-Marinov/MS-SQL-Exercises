a514ff8e7278ba33fb3c4b4d92be2604e0d53921
I-M-Marinov <lcfrrr@gmail.com>
2024-06-12 11:46:30 +0300

    Exercise 9 Solved
    Exercise 9 Solved

--- 12. Exam Preparation - MSSQL - 19 February 2023/Exam-Preparation-19-Feb-2023.sql
+++ 12. Exam Preparation - MSSQL - 19 February 2023/Exam-Preparation-19-Feb-2023.sql
@@ -276,3 +276,12 @@ Example
 				Emerson Matsuuchi	emerson@matsuuchi.com			8.60
 */
 
+SELECT
+CONCAT_WS(' ', c.FirstName, c.LastName) AS FullName,
+c.Email,
+MAX(b.Rating) AS Rating
+FROM Creators AS c
+JOIN CreatorsBoardgames AS cb ON cb.CreatorId = c.Id
+JOIN Boardgames AS b ON b.Id = cb.BoardgameId
+WHERE Email LIKE '%.com'
+GROUP BY c.FirstName, c.LastName, c.Email
