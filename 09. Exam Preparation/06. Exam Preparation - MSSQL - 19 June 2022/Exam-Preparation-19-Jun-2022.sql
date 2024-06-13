ccb2c4f108ff9855f73fe37b18acf2235202e104
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2024-06-13 10:46:17 +0300

    Exercise 3 Solved
    Exercise 3 Solved

--- 09. Exam Preparation/06. Exam Preparation - MSSQL - 19 June 2022/Exam-Preparation-19-Jun-2022.sql
+++ 09. Exam Preparation/06. Exam Preparation - MSSQL - 19 June 2022/Exam-Preparation-19-Jun-2022.sql
@@ -73,9 +73,11 @@ CONSTRAINT FK_Volunteers_VolunteersDepartments FOREIGN KEY (DepartmentId) REFERE
 
 
 /*
-Section 2. DML (10 pts)
-Before you start you have to import "01. DDL_Dataset.sql ". If you have created the structure correctly the data should be successfully inserted. 
+															Section 2. DML (10 pts)
+Before you start you have to import "01. DDL_Dataset.sql ". 
+If you have created the structure correctly the data should be successfully inserted. 
 In this section, you have to do some data manipulations:
+
 2.	Insert
 Let's insert some sample data into the database. Write a query to add the following records into the corresponding tables. All Ids should be auto-generated.
 
@@ -98,3 +100,27 @@ Let's insert some sample data into the database. Write a query to add the follow
 				Tuatara						2021-06-30			   2				4
 
 */
+
+
+INSERT INTO Volunteers ([Name], PhoneNumber, [Address], AnimalId, DepartmentId)
+VALUES ('Anita Kostova', '0896365412', 'Sofia, 5 Rosa str.', 15, 1),
+	   ('Dimitur Stoev', '0877564223', NULL, 42, 4),
+	   ('Kalina Evtimova', '0896321112', 'Silistra, 21 Breza str.', 9, 7),
+	   ('Stoyan Tomov', '0898564100', 'Montana, 1 Bor str.', 18, 8),
+	   ('Boryana Mileva', '0888112233', NULL, 31, 5)
+
+INSERT INTO Animals ([Name], BirthDate, OwnerId, AnimalTypeId)
+VALUES ('Giraffe', '2018-09-21', 21, 1),
+	   ('Harpy Eagle', '2015-04-17', 15, 3),
+	   ('Hamadryas Baboon', '2017-11-02', NULL, 1),
+	   ('Tuatara', '2021-06-30', 2, 4)
+
+
+/*
+3.	Update
+Kaloqn Stoqnov (a current owner, present in the database) came to the zoo to adopt all the animals,
+who don't have an owner. Update the records by putting to those animals the correct OwnerId.
+
+*/
+
+
