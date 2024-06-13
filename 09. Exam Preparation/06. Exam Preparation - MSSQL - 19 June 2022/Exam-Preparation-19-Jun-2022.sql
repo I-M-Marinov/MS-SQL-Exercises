54ae44ee6b03b6674d4de1f07266a4daccb517ed
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2024-06-13 10:41:17 +0300

    Exercise 2 Solved
    Exercise 2 Solved

--- 09. Exam Preparation/06. Exam Preparation - MSSQL - 19 June 2022/Exam-Preparation-19-Jun-2022.sql
+++ 09. Exam Preparation/06. Exam Preparation - MSSQL - 19 June 2022/Exam-Preparation-19-Jun-2022.sql
@@ -70,3 +70,31 @@ DepartmentId INT NOT NULL,
 CONSTRAINT FK_Volunteers_Animals FOREIGN KEY (AnimalId) REFERENCES Animals(Id),
 CONSTRAINT FK_Volunteers_VolunteersDepartments FOREIGN KEY (DepartmentId) REFERENCES VolunteersDepartments(Id)
 )
+
+
+/*
+Section 2. DML (10 pts)
+Before you start you have to import "01. DDL_Dataset.sql ". If you have created the structure correctly the data should be successfully inserted. 
+In this section, you have to do some data manipulations:
+2.	Insert
+Let's insert some sample data into the database. Write a query to add the following records into the corresponding tables. All Ids should be auto-generated.
+
+
+
+				Volunteers
+				Name				PhoneNumber			Address						AnimalId			DepartmentId
+				Anita Kostova		0896365412			Sofia, 5 Rosa str.				15					1
+				Dimitur Stoev		0877564223			null							42					4
+				Kalina Evtimova		0896321112			Silistra, 21 Breza str.			9					7
+				Stoyan Tomov		0898564100			Montana, 1 Bor str.				18					8
+				Boryana Mileva		0888112233			null							31					5
+
+
+				Animals
+				Name						BirthDate			OwnerId			AnimalTypeId
+				Giraffe						2018-09-21			   21				1
+				Harpy Eagle					2015-04-17			   15				3
+				Hamadryas Baboon			2017-11-02			   null			    1
+				Tuatara						2021-06-30			   2				4
+
+*/
