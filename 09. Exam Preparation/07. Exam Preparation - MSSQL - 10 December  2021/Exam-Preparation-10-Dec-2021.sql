a755fd8b075417dcbd0662fb17766c11bd9158c4
I-M-Marinov <lcfrrr@gmail.com>
2024-06-14 11:23:58 +0300

    Exercise 8 Solved
    Exercise 8 Solved

--- 09. Exam Preparation/07. Exam Preparation - MSSQL - 10 December  2021/Exam-Preparation-10-Dec-2021.sql
+++ 09. Exam Preparation/07. Exam Preparation - MSSQL - 10 December  2021/Exam-Preparation-10-Dec-2021.sql
@@ -216,15 +216,6 @@ DestinationId	Start						FullName					AirportName											TicketPrice
 
 */
 
-SELECT * FROM Aircraft
-SELECT * FROM AircraftTypes
-SELECT * FROM Airports
-SELECT * FROM FlightDestinations
-SELECT * FROM Passengers
-SELECT * FROM Pilots
-SELECT * FROM PilotsAircraft
-
-
 SELECT TOP 20
 fd.Id AS DestinationId,
 fd.[Start],
@@ -236,3 +227,51 @@ JOIN Passengers AS p ON p.Id = fd.PassengerId
 JOIN Airports AS a ON a.Id = fd.AirportId
 WHERE DAY(fd.[Start]) % 2 = 0
 ORDER BY fd.TicketPrice DESC, a.AirportName ASC
+
+/*
+8.	Number of Flights for Each Aircraft
+Extract information about all the Aircraft and the count of their FlightDestinations. 
+Display average ticket price (AvgPrice) of each flight destination by the Aircraft, rounded to the second digit. 
+Take only the aircraft with at least 2  FlightDestinations. 
+Order the results by count of flight destinations descending, then by the aircraft's id ascending. 
+
+Required columns:
+
+•	AircraftId
+•	Manufacturer
+•	FlightHours
+•	FlightDestinationsCount
+•	AvgPrice
+
+Example
+
+		AircraftId	Manufacturer		FlightHours	FlightDestinationsCount		AvgPrice
+			13			Safran				849					4				3208.200000
+			80			Lockheed Martin		714					4				1743.140000
+			1			Safran				559					3				.710000
+			8			Safran				527					3				1366.200000
+			25			Northrop Grumman	414					3				452.960000
+			37			GE Aviation			4					3				896.950000
+
+*/
+
+
+SELECT * FROM Aircraft
+SELECT * FROM AircraftTypes
+SELECT * FROM Airports
+SELECT * FROM FlightDestinations
+SELECT * FROM Passengers
+SELECT * FROM Pilots
+SELECT * FROM PilotsAircraft
+
+SELECT 
+a.Id AS AircraftId,
+a.Manufacturer,
+a.FlightHours,
+COUNT (fd.AircraftId) AS FlightDestinationsCount,
+ROUND(AVG(fd.TicketPrice), 2) AS AvgPrice
+FROM Aircraft AS a
+JOIN FlightDestinations AS fd ON fd.AircraftId = a.Id
+GROUP BY a.Id, a.Manufacturer, a.FlightHours
+HAVING COUNT (fd.AircraftId) >= 2
+ORDER BY COUNT (fd.AircraftId) DESC, a.Id ASC
