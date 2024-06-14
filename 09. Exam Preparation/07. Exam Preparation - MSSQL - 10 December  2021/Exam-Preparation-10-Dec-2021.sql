d861b71c7dcd5610183b23767ba1ee44b7b5bbe8
I-M-Marinov <lcfrrr@gmail.com>
2024-06-14 11:10:45 +0300

    Exercise 7 Solved
    Exercise 7 Solved

--- 09. Exam Preparation/07. Exam Preparation - MSSQL - 10 December  2021/Exam-Preparation-10-Dec-2021.sql
+++ 09. Exam Preparation/07. Exam Preparation - MSSQL - 10 December  2021/Exam-Preparation-10-Dec-2021.sql
@@ -181,14 +181,6 @@ Example
 		Crosby			Godlee				Lockheed Martin		F-22 Raptor		271
 */
 
-SELECT * FROM Aircraft
-SELECT * FROM AircraftTypes
-SELECT * FROM Airports
-SELECT * FROM FlightDestinations
-SELECT * FROM Passengers
-SELECT * FROM Pilots
-SELECT * FROM PilotsAircraft
-
 SELECT 
 p.FirstName, 
 p.LastName,
@@ -201,3 +193,46 @@ JOIN Aircraft AS a ON a.Id = pe.AircraftId
 WHERE a.FlightHours < 304 AND a.FlightHours IS NOT NULL
 ORDER BY a.FlightHours DESC, p.FirstName
 
+/*
+7.	Top 20 Flight Destinations
+Select top 20  flight destinations, where Start day is an even number. Extract DestinationId, Start date, passenger's FullName, AirportName, and TicketPrice. 
+Order the result by TicketPrice descending, then by AirportName ascending.
+
+Required columns:
+•	DestinationId
+•	Start
+•	FullName (passenger)
+•	AirportName
+•	TicketPrice
+Example
+
+DestinationId	Start						FullName					AirportName											TicketPrice
+	95		2020-07-02 15:27:47.000		Cullan Dogerty			Kisangani Bangoka International Airport						5048.89
+	9		2020-02-06 22:32:14.000		Lanita Crockatt			Providenciales Airport										4100.49
+	56		2021-02-20 21:04:53.000		Gaye Sillars			Netaji Subhas Chandra Bose International Airport			4002.21
+	55		2021-02-28 13:13:55.000		Zeke Rowston			Sir Seretse Khama International Airport						3700.65
+	32		2020-09-10 01:55:19.000		Jacquelynn Plackstone	Bujumbura International Airport								3690.22
+	38		2020-11-28 17:58:40.000		Jeralee Tue				WinnipegJames Armstrong Richardson International Airport	3390.81
+
+*/
+
+SELECT * FROM Aircraft
+SELECT * FROM AircraftTypes
+SELECT * FROM Airports
+SELECT * FROM FlightDestinations
+SELECT * FROM Passengers
+SELECT * FROM Pilots
+SELECT * FROM PilotsAircraft
+
+
+SELECT TOP 20
+fd.Id AS DestinationId,
+fd.[Start],
+p.FullName,
+a.AirportName,
+fd.TicketPrice
+FROM FlightDestinations AS fd
+JOIN Passengers AS p ON p.Id = fd.PassengerId
+JOIN Airports AS a ON a.Id = fd.AirportId
+WHERE DAY(fd.[Start]) % 2 = 0
+ORDER BY fd.TicketPrice DESC, a.AirportName ASC
