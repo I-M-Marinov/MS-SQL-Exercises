13d2a2bdb510ddb8559b4cabf170c8832935a1a8
I-M-Marinov <lcfrrr@gmail.com>
2024-06-16 15:35:10 +0300

    Exercise 2 Solved
    Exercise 2 Solved

--- 10. Regular Exam - 16 June 2024/Regular-Exam-16-June-2024.sql
+++ 10. Regular Exam - 16 June 2024/Regular-Exam-16-June-2024.sql
@@ -63,3 +63,92 @@ CONSTRAINT PK_LibrariesBooks PRIMARY KEY (LibraryId, BookId),
 CONSTRAINT FK_LibrariesBooks_Libraries FOREIGN KEY (LibraryId) REFERENCES Libraries(Id),
 CONSTRAINT FK_LibrariesBooks_Books FOREIGN KEY (BookId) REFERENCES Books(Id)
 )
+
+/*
+				Section 2. DML (10 pts)
+				Before you start, you have to import "Dataset.sql ". If you have created the structure correctly, the data should be successfully inserted.
+				In this section, you have to do some data manipulations:
+
+2.	Insert
+
+Let's insert some sample data into the database. Write a query to add the following records to the corresponding tables. All IDs (Primary Keys) should be auto-generated.
+
+
+						Contacts
+Id	Email	PhoneNumber	PostAddress	Website
+21	NULL	NULL	NULL	NULL
+22	NULL	NULL	NULL	NULL
+23	'stephen.king@example.com'	'+4445556666'	'15 Fiction Ave, Bangor, ME'	'www.stephenking.com'
+24	'suzanne.collins@example.com'	'+7778889999'	'10 Mockingbird Ln, NY, NY'	'www.suzannecollins.com'
+
+						Authors
+Id	Name	ContactId
+16	'George Orwell'	21
+17	'Aldous Huxley'	22
+18	'Stephen King'	23
+19	'Suzanne Collins'	24
+
+							Books
+Id	Title	YearPublished	ISBN	AuthorId	GenreId
+36	'1984'	1949	'9780451524935'	16	2
+37	'Animal Farm'	1945	'9780451526342'		16	2
+38	'Brave New World'	1932	'9780060850524'	17	2
+39	'The Doors of Perception'	1954	'9780060850531'	17	2
+40	'The Shining'	1977	'9780307743657'	18	9
+41	'It'	1986	'9781501142970'	18	9
+42	'The Hunger Games'	2008	'9780439023481'	19	7
+43	'Catching Fire'	2009	'9780439023498'	19	7
+44	'Mockingjay'	2010	'9780439023511'	19	7
+
+LibrariesBooks
+LibraryId	BookId
+	1		  36
+	1		  37
+	2		  38
+	2		  39
+	3		  40
+	3		  41
+	4		  42
+	4		  43
+	5		  44
+
+
+*/
+
+
+INSERT INTO Contacts (Email, PhoneNumber, PostAddress, Website)
+VALUES (NULL, NULL, NULL, NULL),
+	   (NULL, NULL, NULL, NULL),
+	   ('stephen.king@example.com', '+4445556666', '15 Fiction Ave, Bangor, ME', 'www.stephenking.com'),
+	   ('suzanne.collins@example.com', '+7778889999', '10 Mockingbird Ln, NY, NY', 'www.suzannecollins.com')
+
+
+INSERT INTO Authors([Name], ContactId)
+VALUES ('George Orwell', 21),
+	   ('Aldous Huxley', 22),
+	   ('Stephen King', 23),
+	   ('Suzanne Collins', 24)
+
+INSERT INTO Books(Title, YearPublished, ISBN, AuthorId, GenreId)
+VALUES ('1984', 1949, '9780451524935', 16 ,2),
+       ('Animal Farm', 1945, '9780451526342', 16 ,2),
+       ('Brave New World', 1932, '9780060850524', 17 ,2),
+       ('The Doors of Perception', 1954, '9780060850531', 17, 2),
+       ('The Shining', 1977, '9780307743657', 18, 9),
+	   ('It', 1986, '9781501142970', 18, 9),
+	   ('The Hunger Games', 2008, '9780439023481', 19, 7),
+	   ('Catching Fire', 2009, '9780439023498', 19, 7),
+	   ('Mockingjay', 2010, '9780439023511', 19, 7)
+
+
+
+INSERT INTO LibrariesBooks(LibraryId, BookId)
+VALUES (1, 36),
+       (1, 37),
+	   (2, 38),
+	   (2, 39),
+	   (3, 40),
+	   (3, 41),
+	   (4, 42),
+	   (4, 43),
+	   (5, 44);
