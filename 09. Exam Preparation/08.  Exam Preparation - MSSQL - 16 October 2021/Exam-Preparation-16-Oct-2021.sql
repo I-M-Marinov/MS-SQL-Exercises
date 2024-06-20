d3ddd59d3b7ae1493651adce41bd7a906cf85391
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2024-06-21 00:10:59 +0300

    Final fix on Exercise 10
    Final fix on Exercise 10

--- 09. Exam Preparation/08.  Exam Preparation - MSSQL - 16 October 2021/Exam-Preparation-16-Oct-2021.sql
+++ 09. Exam Preparation/08.  Exam Preparation - MSSQL - 16 October 2021/Exam-Preparation-16-Oct-2021.sql
@@ -313,10 +313,11 @@ SELECT * FROM ClientsCigars
 SELECT
 c.LastName,
 AVG(s.[Length]) AS CigarLength,
-CEILING(MAX(s.RingRange)) AS CigarRingRange
+CEILING(AVG(s.RingRange)) AS CigarRingRange
 FROM Clients AS c 
 JOIN ClientsCigars AS cc ON c.Id = cc.ClientId
 JOIN Cigars AS ci ON ci.Id = cc.CigarId
 JOIN Sizes AS s ON s.Id = ci.SizeId
 GROUP BY c.LastName
 ORDER BY CigarLength DESC;
+
